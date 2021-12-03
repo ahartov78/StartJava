7a28a9782fc135febaf6cee1fd947417cbcbe210
ahartov78 <95459603+ahartov78@users.noreply.github.com>
2021-12-02 21:16:11 -0900

    Add files via upload

---
+++ Lesson_1/Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte a = 8;
+        System.out.println("Core = " + a);
+        short b = 16;
+        System.out.println("RAM = " + b);
+        int c = 64;
+        System.out.println("Bit = " + c);
+        long d = 1080;
+        System.out.println("Resolution = " + d);
+        float e = 1.800f;
+        System.out.println("CPU = " + e);
+        double f = 2.300;
+        System.out.println("CPU1 = " + f);
+        char g = 'A';
+        System.out.println("Name = " + g);
+        boolean h = false;
+        System.out.println("DVD = " + h);
+    }
+}
